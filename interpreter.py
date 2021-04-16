30b1e6645e3665a1e439aefe9643861e7e86de32
alex <ailyushonak@gmail.com>
2021-04-16 15:09:14 +0200

    refactoring

--- interpreter.py
+++ interpreter.py
@@ -1,5 +1,7 @@
 
 import os
+import sys
+import re
 from datetime import datetime
 
 # main class with the interpreter code
@@ -10,22 +12,22 @@ class Interpreter:
 
     def reset(self):
         self.memory = [0, 0, 0]
-        self.currentcell = 0
+        self.pointer = 0
 
     # opens and runs a bf file
     def run_file(self, fname):
         if not os.path.isfile(fname):
             print(f"Error: File '{fname}' not found")
             return
-        code = self._get_file_content(fname)
-        self.run_code(code)
+        code = self._read_file(fname)
+        self.run_code(code, extra_commands=True)
 
     # gets text in a file
-    def _get_file_content(self, fname):
+    def _read_file(self, fname):
         f = open(fname, "r")
         cont = f.read()
         f.close()
-        return cont
+        return re.sub(";.*", "", cont)
 
     def _get_datetime(self):
         return datetime.now().strftime("%a, %e.%m - %R:%S")
@@ -35,34 +37,33 @@ class Interpreter:
         # greeting
         print("Welcome to the Brainfuck console!")
         print(f"Brainfuck {self.VERSION}, {self._get_datetime()} on {os.name}")
-        inp = input("bf> ")
-        # enter ":q" to exit
-        while inp != ":q":
-            # ":r" resets the interpreter
-            if inp == ":r":
-                self.reset()
-            # ":e" to run a file
-            elif inp == ":e":
-                fname = input("filename> ")
-                self.run_file(fname)
-            elif inp == ":l":
-                if os.name == "nt": os.system("cls")
-                else: os.system("clear")
-            # just run raw bf code
-            else:
-                try:
-                    self.run_code(inp)
-                except KeyboardInterrupt:
-                    print("Execution terminated by user.")
-                    self.print_debug_info()
-            print()
+        while True:
             try:
-                inp = input("bf> ")
+                self.run_code(input("\nbf> "), extra_commands=True)
             except KeyboardInterrupt:
-                print("Program terminated by user")
+                print("Terminated by user")
                 exit(1)
 
-    def run_code(self, code: str):
+    def run_extra(self, command: str):
+        if command.startswith("q") or command.startswith("quit") or command.startswith("exit"):
+            exit(0)
+        if command.startswith("r"):
+            self.reset()
+            return
+        if command.startswith("e"):
+            try: fname = command.split()[1]
+            except IndexError: print("You need to provide an argument to ':e'")
+            else: self.run_file(fname)
+        if command.startswith("l"):
+            if os.name == "nt": os.system("cls")
+            else: os.system("clear")
+            return
+        if command.startswith("help"):
+            self.print_help()
+            return
+        print(f"Command '{command.split()[0]}' was not found")
+
+    def _run(self, code):
         pointer = 0
         return_to = []
         returned = False
@@ -83,8 +84,18 @@ class Interpreter:
                 del return_to[-1]
                 returned = False
                 pointer += 1
-            #print(f"after: {status}")
-            #print(return_to)
+            # print(f"after: {status}")
+            # print(return_to)
+
+    def run_code(self, code: str, extra_commands=False):
+        if extra_commands:
+            for line in code.split("\n"):
+                if line.startswith(":"):
+                    self.run_extra(line[1:])
+                    continue
+                self._run(line)
+            return
+        self._run(code)
 
     # runs one bf command
     # status:
@@ -94,58 +105,51 @@ class Interpreter:
     # 3 - skipping a loop
     def execute(self, command):
         status = 0
-        if command == '+':
-            self.modify_current_cell(True)
-        if command == '-':
-            self.modify_current_cell(False)
+        if command == '+': self.modify_current_cell(True)
+        if command == '-': self.modify_current_cell(False)
+        if command == ',': self._input()
+        if command == '.': self._output()
+        if command == "#": self.print_debug_info()
         if command == '<':
-            self.currentcell -= 1
-            if self.currentcell < 0:
-                self.currentcell = 0
+            self.pointer -= 1
+            if self.pointer < 0: self.pointer = 0
         if command == '>':
-            self.currentcell += 1
-        if command == ',':
-            self._input()
-        if command == '.':
-            self._output(self.memory[self.currentcell])
-        if command == "#":
-            self.print_debug_info()
+            self.pointer += 1
+            self._fill_cells()
         if command == '[':
-            if self.memory[self.currentcell] == 0: status = 3
+            if self.memory[self.pointer] == 0: status = 3
             else: status = 1
         if command == ']':
             status = 2
-        #print(f"in exe: {status}")
         return status
 
     # +/- bf commands
     def modify_current_cell(self, mode=True):
-        try:
-            if mode:
-                self.memory[self.currentcell] += 1
-                return
-            self.memory[self.currentcell] -= 1
-            if self.memory[self.currentcell] < 0: self.memory[self.currentcell] = 0
-        except IndexError:
-            if mode:
-                for cell in range(self.currentcell - len(self.memory)):
-                    self.memory.append(0)
-                self.memory.append(1)
-                return
-            for cell in range(self.currentcell + 1 - len(self.memory)):
-                self.memory.append(0)
+        self._fill_cells()
+        if mode:
+            self.memory[self.pointer] += 1
+            return
+        self.memory[self.pointer] -= 1
+        if self.memory[self.pointer] < 0: self.memory[self.pointer] = 0
 
     # prints debug information
     def print_debug_info(self):
+        print()
         print("==========DEBUG==========")
         print(f"memory: {self.memory}")
-        print(f"pointer: {self.currentcell}")
+        print(f"pointer: {self.pointer}")
         print("=========================")
+        print()
+
+    def print_help(self):
+        print("Help was not implemented yet")
 
     # prints a char to the terminal (bf .)
-    def _output(self, number):
+    def _output(self):
+        number = self.memory[self.pointer]
         if number < 32: number = 32
-        print(chr(number), end="")
+        #print(chr(number), end="")
+        sys.stdout.write(chr(number))
 
     # input a char (bf ,)
     def _input(self):
@@ -153,12 +157,12 @@ class Interpreter:
         if len(inp) > 1:
             inp = inp[0]
             print("?> " + inp)
-        try:
-            self.memory[self.currentcell] = ord(inp)
-        except IndexError:
-            self._fill_cells()
-            self.memory[self.currentcell] = ord(inp)
+        if len(inp) < 1:
+            self.memory[self.pointer] = 0
+            return
+        self._fill_cells()
+        self.memory[self.pointer] = ord(inp)
 
     def _fill_cells(self):
-        for cell in range(self.currentcell + 1 - len(self.memory)):
+        for cell in range(self.pointer + 1 - len(self.memory)):
             self.memory.append(0)
